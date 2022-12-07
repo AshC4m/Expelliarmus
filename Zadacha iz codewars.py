4885c0236bcf42cc002bf518c6308690f01ca23f
AshC4m <lykinsergey@yandex.ru>
2022-12-07 15:07:14 +0300

    сделал задачу из codewars

--- Zadacha iz codewars.py
+++ Zadacha iz codewars.py
@@ -1,9 +1,14 @@
-#https://www.codewars.com/kata/5a023c426975981341000014
-# You are given two interior angles (in degrees) of a triangle.
-#
-# Write a function to return the 3rd.
-#
-# Note: only positive integers will be tested.
+# https://www.codewars.com/kata/577a98a6ae28071780000989
+tst1 = [4, 89, 2398, 294, 14, 988]
+tst2 = [44, 66, 898, 33, 9999, 15]
 
-def other_angle(a, b):
-    return 180 - (a + b)
+# using max() + min() + "+" operator
+# maximum and minimum values in two lists
+max1 = max(tst1)
+min1 = min(tst1)
+max2 = max(tst2)
+min2 = min(tst2)
+print("максимум в первом списке : " + str(max1))
+print("минимум в первом списке : " + str(min1))
+print("максимум во втором списке : " + str(max2))
+print("минимум во втором списке : " + str(min2))
