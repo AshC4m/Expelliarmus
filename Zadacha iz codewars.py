4019acc7c34959d5e7f9dff796884b453108ba08
AshC4m <lykinsergey@yandex.ru>
2022-12-07 15:24:39 +0300

    сделал задачу из codewars

--- Zadacha iz codewars.py
+++ Zadacha iz codewars.py
@@ -1,11 +1,7 @@
-# https://www.codewars.com/kata/577a98a6ae28071780000989
-tst1 = [4, 89, 2398, 294, 14, 988]
-tst2 = [44, 66, 898, 33, 9999, 15]
-max1 = max(tst1)
-min1 = min(tst1)
-max2 = max(tst2)
-min2 = min(tst2)
-print("максимум в первом списке : " + str(max1))
-print("минимум в первом списке : " + str(min1))
-print("максимум во втором списке : " + str(max2))
-print("минимум во втором списке : " + str(min2))
+# https://www.codewars.com/kata/5899dc03bc95b1bf1b0000ad
+
+lsst=[55,12,3,5,7,2,]
+def invert(lsst):
+    return [ n * -1 for n in lsst ] if len(lsst) > 0 else []
+print(invert(lsst))
+
